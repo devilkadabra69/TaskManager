d14e272c2486aaca262c577c81700821c0ed02b5
ADARSH KUMAR SINGH <mrrajputasr731@gmail.com>
2024-06-02 04:48:20 +0530

    _

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,25 +11,3 @@ const verifyJwt = asyncHandler(async (req, res, next) => {
     }
 })
 
-const login = asyncHandler(async (req, res, next) => {
-    try {
-        // logic to write login
-        const { email, password, userName } = req.body;
-        if (!(email || userName)) {
-            return new ApiError(401, "Please provide email or username");
-        }
-
-        if (!password) {
-            return new ApiError(401, "Please provide password");
-        }
-
-        const user = await User.findOne({ $or: [{ email }, { userName }] });
-        if (!user) {
-            return new ApiError(401, "Invalid Credentials");
-        }
-
-
-    } catch (error) {
-        return new ApiError(501, `Unauthorized or Something went wrong :: Line 33 auth.middleware.js ::\n ${error?.message}`, error);
-    }
-})
